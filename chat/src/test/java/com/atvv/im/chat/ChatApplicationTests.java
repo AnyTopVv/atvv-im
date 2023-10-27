5eaa1c84141658d7f1c12ae8e6d49342487cfb3c
lzy <848539845@qq.com>
2023-10-27 11:25:54 +0800

    feat: chat分包

---
+++ chat/src/test/java/com/atvv/im/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atvv.im.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
